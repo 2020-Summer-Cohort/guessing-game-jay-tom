836d7c347ec3624b920ce58e514cd70f4b0c2509
Tom <tomrambo95@yahoo.com>
2020-05-14 12:50:11 -0400

    guessing game

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,6 +7,40 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Guessing Game!");
+
+            bool keepThinking = true;
+            while (keepThinking)
+            {
+                IterationOne();
+                Console.WriteLine("Do you want to play again?\n");
+                string userChoice = Console.ReadLine().ToLower();
+
+                if (userChoice != "yes")
+                    keepThinking = false;
+
+            }
+
+            //method
+            static void IterationOne()
+            {
+
+
+                Console.WriteLine("Guess a number 1-10");
+                string inputValue = Console.ReadLine();
+
+                if (inputValue == "7")
+                {
+                    Console.WriteLine("You Win!");
+
+                }
+                else
+                {
+                    Console.WriteLine("You Lose!");
+                }
+                Console.WriteLine("Press Enter To Return");
+
+            }
         }
     }
 }
+
